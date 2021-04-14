23fa0cd494f4c4323cbc039c93cc09d6a972b852
Adrian <correo.d.adrian@gmail.com>
2021-04-13 21:15:39 -0500

    RETO parcial

---
+++ ConsoleApp1/ConsoleApp1/List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface List<T>
+    {
+        void add(T data);
+
+        T get(int index);
+
+        void delete(int index);
+
+        int getSize();
+
+        Iterator<T> getForwardIterator();
+
+        Iterator<T> getReverseIterator();
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            List<int> lista1 = new ArrayList<int>();
+            List<String> lista2 = new ArrayList<String>();
+
+            lista1.add(1);
+            lista1.add(4);
+            lista1.add(3);
+            lista1.add(7);
+
+            lista2.add(" hola");
+            lista2.add(" Object ");
+            lista2.add(" Oriented ");
+            lista2.add(" Programming ");
+
+            lista1.delete(3);
+
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("El tamaño es:" + lista1.getSize());
+
+            Iterator<int> it = lista1.getForwardIterator();
+
+            while (it.hasNext())
+            {
+                int element = it.next();
+
+                Console.WriteLine("Dato: " + element);
+            }
+
+            Console.WriteLine("---------------------");
+
+            it = lista1.getReverseIterator();
+
+            while (it.hasNext())
+            {
+                int element = it.next();
+
+                Console.WriteLine("Dato: " + element);
+            }
+
+            Console.WriteLine("---------------------");
+
+            Iterator<String> itStrings = lista2.getReverseIterator();
+
+            while (itStrings.hasNext())
+            {
+                Console.WriteLine(itStrings.next());
+            }
+        }
+	}
+}
